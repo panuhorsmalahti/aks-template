c75c3ca50f71bc873dbeb1980b16c181a7591206
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2020-01-06 17:54:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# AKS template
+
+This repository hosts an example AKS template consisting of:
+
+* Azure Kubernetes Service
+* Azure Container Registry
+* Kubernetes / Docker / Docker Compose
+* Terraform
+* Node.js + Nest.js + TypeScript + Swagger + Prettier
+
+Test instance hosted at:
+
+* http://52.173.134.26:3000/ping
+* phstestcontainerregistry.azurecr.io
---
+++ infrastructure/output.tf
@@ -0,0 +1,39 @@
+output "client_key" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+}
+
+output "client_certificate" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+}
+
+output "cluster_username" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
+}
+
+output "cluster_password" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+}
+
+output "kube_config" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
+
+output "host" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+}
+
+output "acr_username" {
+    value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_password" {
+    value = azurerm_container_registry.acr.admin_password
+}
+
+output "acr_login" {
+    value = azurerm_container_registry.acr.login_server
+}
